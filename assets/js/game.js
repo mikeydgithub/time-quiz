d1314508f47b6da53d645845416c2dd40c3be0ed
Michael Diamond <94988620+mikeydgithub@users.noreply.github.com>
2022-01-23 13:07:35 -0500

    update

--- assets/js/game.js
+++ assets/js/game.js
@@ -79,7 +79,7 @@ getNewQuestion = () => {
     if(availableQuestions.length === 0 || questionCounter > maxQuestions) {
         localStorage.setItem('mostRecentScore', score);
 
-        return window.location.assign('endpage.html');
+        return window.location.assign('/endpage.html');
     }
 
     questionCounter++;
